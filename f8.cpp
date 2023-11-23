55e515d927cfed21a1300a78e82304bfcf28f592
Run-J <jirun040403@hotmail.com>
2023-11-23 17:42:32 -0500

    This time, I declared a variable with struct as its data type and copied the contents of argv to the struct variable using strcpy.

--- f8.cpp
+++ f8.cpp
@@ -1,6 +1,6 @@
 //Filename: f8.cpp
 //Project: Focus 8
-//Programmer: Run Ji
+//Programmer:
 //First version: 11/19/2023
 //Description: Users can enter the text they want to store, the number of times they want to store it, and the path to store it.
 
