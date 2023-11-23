69883fbf6a5114e0a1d6032373f22b6823f32d33
Run-J <jirun040403@hotmail.com>
2023-11-23 17:51:24 -0500

    This time, I declared a variable with struct as its data type and copied the contents of argv to the struct variable using strcpy.

--- Project1/Project1/f8.cpp
+++ Project1/Project1/f8.cpp
@@ -1,8 +1,4 @@
-//Filename: f8.cpp
-//Project: Focus 8
-//Programmer: Run Ji
-//First version: 11/19/2023
-//Description: Users can enter the text they want to store, the number of times they want to store it, and the path to store it.
+
 
 #include<stdio.h>
 #include<stdlib.h>
@@ -21,3 +17,31 @@ struct MyData
 	char directoryPath[pathLenth];
 	char filename[filenameLenth];
 };
+
+int main(int argc, char* argv[])
+{
+	if (argc != 5) //check how many arguments are on the command line
+	{
+		fprintf(stderr, "Error: Not four arguments.\n");
+		return 1;
+	}
+
+
+	struct MyData myArgs = { 0,"","","" };
+	myArgs.HowMany = atoi(argv[1]);
+	if (myArgs.HowMany <= 0) //check first argument whether > 0
+	{
+		fprintf(stderr, "Error: The first argument should be positive integer.\n");
+		return 1;
+	}
+
+
+	if (strlen(argv[2]) > txtContentLenth - 1 || strlen(argv[3]) > pathLenth - 1 || strlen(argv[4]) > filenameLenth - 1) //check string arguments lenth without \0
+	{
+		fprintf(stderr, "Error: The argument exceeds the length limited\n");
+		return 1;
+	}
+
+	strcpy(myArgs.theText, argv[2]); //copy arguments input to myArgs
+	strcpy(myArgs.directoryPath, argv[3]);
+	strcpy(myArgs.filename, argv[4]);
