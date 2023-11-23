c27db3ecefa8ca48be5d7de6916bdb9f41b9be6c
Run-J <jirun040403@hotmail.com>
2023-11-23 18:02:42 -0500

    Use command argument line as input, and write it into a file

--- f8.cpp
+++ f8.cpp
@@ -22,3 +22,57 @@ struct MyData
 	char filename[filenameLenth];
 };
 
+int main(int argc, char* argv[])
+{
+	if (argc != 5) //check how many arguments are on the command line
+	{
+		fprintf(stderr, "Error: Not four arguments.\n");
+		return 1;
+	}
+
+
+	struct MyData myArgs = { 0,"","","" };
+	myArgs.HowMany = atoi(argv[1]);
+	if (myArgs.HowMany <= 0) //check first argument whether > 0
+	{
+		fprintf(stderr, "Error: The first argument should be positive integer.\n");
+		return 1;
+	}
+
+	
+	if (strlen(argv[2]) > txtContentLenth-1 || strlen(argv[3]) > pathLenth-1 || strlen(argv[4]) > filenameLenth-1) //check string arguments lenth without \0
+	{
+		fprintf(stderr, "Error: The argument exceeds the length limited\n");
+		return 1;
+	}
+
+	strcpy(myArgs.theText, argv[2]); //copy arguments input to myArgs
+	strcpy(myArgs.directoryPath, argv[3]);
+	strcpy(myArgs.filename, argv[4]);
+
+	char fullLocation[pathLenth + filenameLenth-1] = "";
+	sprintf(fullLocation, "%s\\%s", myArgs.directoryPath, myArgs.filename); //variable to contain the full location of the file
+
+	FILE* txtfp = fopen(fullLocation, "w");
+	if (txtfp == NULL)
+	{
+		fprintf(stderr, "Error: Failed to open the file\n");
+		return 1;
+	}
+
+	for (int i = 0; i < myArgs.HowMany; i++)
+	{
+		if (fprintf(txtfp, "%s\n", myArgs.theText) < 0)
+		{
+			fprintf(stderr, "Error: Failed write to the file\n");
+			return 1;
+		}
+	}
+
+	if (fclose(txtfp) != 0)
+	{
+		fprintf(stderr, "Error: Failed to close the File\n");
+	}
+
+	return 0;
+}
