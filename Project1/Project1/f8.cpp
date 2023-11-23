afc462227640a679940b9455a6b95474903f39a2
Run-J <jirun040403@hotmail.com>
2023-11-23 17:55:20 -0500

    This time, I opened the file using fopen and wrote the contents to the file using fprintf, and finally I closed the file using fclose while I did all the error checking.

--- Project1/Project1/f8.cpp
+++ Project1/Project1/f8.cpp
@@ -1,3 +1,8 @@
+//Filename: f8.cpp
+//Project: Focus 8
+//Programmer: Run Ji
+//First version: 11/19/2023
+//Description: Users can enter the text they want to store, the number of times they want to store it, and the path to store it.
 
 
 #include<stdio.h>
@@ -40,8 +45,27 @@ int main(int argc, char* argv[])
 	{
 		fprintf(stderr, "Error: The argument exceeds the length limited\n");
 		return 1;
+	
+	sprintf(fullLocation, "%s\\%s", myArgs.directoryPath, myArgs.filename); //variable to contain the full location of the file
+
+	FILE* txtfp = fopen(fullLocation, "w");
+	if (txtfp == NULL)
+	{
+		fprintf(stderr, "Error: Failed to open the file\n");
+		return 1;
 	}
 
-	strcpy(myArgs.theText, argv[2]); //copy arguments input to myArgs
-	strcpy(myArgs.directoryPath, argv[3]);
-	strcpy(myArgs.filename, argv[4]);
+	for (int i = 0; i < myArgs.HowMany; i++)
+	{
+		if (fprintf(txtfp, "%s\n", myArgs.theText) < 0)
+		{
+			fprintf(stderr, "Error: Failed write to the file\n");
+			retu
+
+	if (fclose(txtfp) != 0)
+	{
+		fprintf(stderr, "Error: Failed to close the File\n");
+	}
+
+	return 0;
+}
